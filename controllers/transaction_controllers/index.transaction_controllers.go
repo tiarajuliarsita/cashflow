c0f9c7131bdf6f323d82042b90f21d5421efde48
tiarajuliarsita <tiarajuliarsita@gmail.com>
2023-06-22 11:37:31 +0800

    feat : add feature transfer

--- controllers/transaction_controllers/index.transaction_controllers.go
+++ controllers/transaction_controllers/index.transaction_controllers.go
@@ -1,7 +1,6 @@
 package transaction_controllers
 
 import (
-	"fmt"
 	"mini_Atm/database"
 	"mini_Atm/models"
 	"mini_Atm/request/transaction_request"
@@ -24,6 +23,24 @@ func TransferTransaction(c *gin.Context) {
 		return
 	}
 
+	// check account number of recipient user
+	recipientUser := new(models.Users)
+	err = database.DB.Table("users").Where("account_number = ?", trfReq.RecipientAccountNumber).Find(&recipientUser).Error
+	if err != nil {
+		c.JSON(500, gin.H{
+			"message": "internal server error",
+		})
+		return
+	}
+
+	if trfReq.RecipientAccountNumber != recipientUser.AccountNumber {
+		c.AbortWithStatusJSON(404, gin.H{
+			"message": "invalid recipient account number",
+		})
+		return
+	}
+
+	// check pin of sender user
 	err = database.DB.Table("users").Where("id = ?", id).Find(&senderUser).Error
 	if err != nil {
 		c.JSON(500, gin.H{
@@ -45,8 +62,7 @@ func TransferTransaction(c *gin.Context) {
 		return
 	}
 
-
-	// trf := new(transaction_request.TranferReq)
+	// set values
 	idInt, _ := strconv.Atoi(id)
 	trfExist := models.Transfer{
 		UserID:                 idInt,
@@ -54,8 +70,7 @@ func TransferTransaction(c *gin.Context) {
 		Amount:                 trfReq.Amount,
 		RecipientAccountNumber: trfReq.RecipientAccountNumber,
 	}
-	fmt.Println("ini datanya", trfExist)
-
+	// insert to database
 	err = database.DB.Table("transfer").Create(&trfExist).Error
 	if err != nil {
 		c.JSON(500, gin.H{
@@ -63,8 +78,41 @@ func TransferTransaction(c *gin.Context) {
 		})
 		return
 	}
+	// change the balance on sender user
+	currentBalanceSenderUser := senderUser.Balance - trfReq.Amount
+	if trfReq.Amount > senderUser.Balance {
+		c.AbortWithStatusJSON(400, gin.H{
+			"message": "your balance is not enough",
+		})
+		return
+	}
+	senderUser.Balance = currentBalanceSenderUser
+	err = database.DB.Table("users").Where("id = ?", id).Update("balance", senderUser.Balance).Error
+	if err != nil {
+		c.JSON(500, gin.H{
+			"message": "internal server error",
+		})
+		return
+	}
+
+	//change the balance on recipient user
+
+	currentBalanceRecipientUser := recipientUser.Balance + trfReq.Amount
+	recipientUser.Balance = currentBalanceRecipientUser
+	err = database.DB.Table("users").Where("account_number = ?", trfReq.RecipientAccountNumber).Update("balance", recipientUser.Balance).Error
+	if err != nil {
+		c.JSON(500, gin.H{
+			"message": "internal server error",
+		})
+		return
+	}
+
 	c.JSON(200, gin.H{
-		"message":     "transfer succesfully",
-		"transaction": trfReq,
+		"message": "transfer succesfully",
+		"transaction": gin.H{
+			"recipient account number": trfReq.RecipientAccountNumber,
+			"amount":                   trfReq.Amount,
+			"currentBalance":           senderUser.Balance,
+		},
 	})
 }
